ae4be8489b6cf4cd6cc0ec5d1faf715a827fe9ce
Ilia Meerovich <lirikazz777@gmail.com>
2023-09-18 14:34:52 +1100

    a3

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,18 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function(mod) {
+var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const products = [{ title: 'tomato' }, { title: 'orange' }];
+const addresses = [{ value: 'Druzbi 6' }, { value: 'Lenina 234' }];
+app.get('/products', (req, res) => {
+    res.send(products);
+});
+app.get('/addresses', (req, res) => {
+    res.send(addresses);
 });
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,15 @@ import express, {Request, Response} from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+const products = [{title: 'tomato'}, {title: 'orange'}]
+const addresses = [{value: 'Druzbi 6'}, {value: 'Lenina 234'}]
+
+app.get('/products', (req: Request, res: Response) => {
+  res.send(products);
+})
+
+app.get('/addresses', (req: Request, res: Response) => {
+  res.send(addresses);
 })
 
 app.listen(port, () => {
